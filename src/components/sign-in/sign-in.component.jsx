054f9847b211f1876c3326d196f6334e2ed2d009
Darlington Otchere <darlington.otchere@marginsgroup.com>
2021-08-17 17:02:37 +0000

    Implemented sign-in with email and password using firestore

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -15,10 +15,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: ''});
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     handleChange = event => {
@@ -37,7 +44,7 @@ class SignIn extends React.Component {
                     <FormInput 
                         type="email" 
                         name="email" 
-                        handleChange={this.onChange} 
+                        onChange={this.handleChange} 
                         value={this.state.email} 
                         required 
                         label='email'
@@ -46,7 +53,7 @@ class SignIn extends React.Component {
                     <FormInput 
                         type="password" 
                         name="password" 
-                        handleChange={this.onChange} 
+                        onChange={this.handleChange} 
                         value={this.state.password} 
                         required 
                         label='password'
